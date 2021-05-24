104f337dc3154180f50cb3b7b387a2ff3d51d977
Micha Gorelick <mynameisfiber@gmail.com>
2021-05-24 13:45:58 +0200

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="followthemoney-compare",
-    version="0.2.0",
+    version="0.3.0",
     author="Organized Crime and Corruption Reporting Project",
     author_email="data@occrp.org",
     url="https://github.com/alephdata/followthemoney-compare/",
